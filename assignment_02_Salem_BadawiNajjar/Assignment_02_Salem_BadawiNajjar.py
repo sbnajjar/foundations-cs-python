e200b8e84c8307c7f276d499707dd13b8116f0a0
sbnajjar <sbnajjar@gmail.com>
2023-07-15 04:42:44 +0300

    Choice 2 completed

--- assignment_02_Salem_BadawiNajjar/Assignment_02_Salem_BadawiNajjar.py
+++ assignment_02_Salem_BadawiNajjar/Assignment_02_Salem_BadawiNajjar.py
@@ -3,7 +3,7 @@
 import os
 
 
-def count_Digits(number, iteration):
+def countDigits(number, iteration):
     
     remainder = number % (10**iteration)
     
@@ -11,24 +11,61 @@ def count_Digits(number, iteration):
        return iteration
     else:
        iteration +=1
-       return count_Digits(number, iteration) 
+       return countDigits(number, iteration) 
 
 def Choice1():
 
-    
-    number = int(input("\nEnter an integer : "))
-      
+    while True:
+        number = input("Enter an integer : ")
+        try:
+            number = int(number)
+            break
+        except ValueError:
+            print("")
 
-    answer = count_Digits(number, iteration =1) 
+    answer = countDigits(number, iteration =1) 
 
     print("\nThe number of digits is: ", answer)
     
     input("\nPress any Key to continue")
 
 
+def getMax(max_number,index):
+
+    if max_number < integer_list[index]:
+
+        max_number = integer_list[index]
+
+    if index < max_index:
+       return getMax(max_number,index+1)
+    else:
+       return max_number
+
 def Choice2():
 
-    number = int(input("\nEnter a list of integers to find the max : "))
+    invalid_input = True
+    while invalid_input:
+        user_input = input("\nEnter a list of integers, separated by commas: ")
+        user_list = user_input.split(",")
+        global integer_list
+        integer_list = []
+        invalid_input = False
+        for item in user_list:
+            try:
+                integer_list.append(int(item))
+            except ValueError:
+                print("Invalid input:", item, "is not an integer.")
+                invalid_input = True
+
+    
+    global max_index
+    max_index = len(integer_list)-1
+    index=0
+    max_number = integer_list[0]
+
+    max_number = getMax(max_number,index)
+
+    print("Maximum integer is: ", max_number)
 
     input("\nPress any Key to continue")
 
